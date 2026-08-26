f03ebcf5aa970e9d812ba73906c4c17b3170fdea
David Lima <davidblima@protonmail.com>
2026-08-26 13:59:26 -0300

    fix: corrige CORS e smoke tests

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,6 +22,8 @@ app.add_middleware(
         "http://127.0.0.1:5500",
         "http://localhost:8080",
         "http://127.0.0.1:8080",
+        "https://escala-tech.davidblima.com.br",
+        "http://escala-tech.davidblima.com.br",
     ],
     allow_credentials=True,
     allow_methods=["*"],
